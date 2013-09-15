dd18f2f55c5c7dd46a2cc744caecac6ae7498e67
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-15 13:08:48 +0900

    fix_deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,19 @@
 require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 
-set :application, "jpa_database"
+set :application, "rails_deploy_test"
 
 set :scm, :git
-set :repository,  "git@bitbucket.org:masayukioguni/jpa_database.git"
+set :repository,  "git@github.com:masayukioguni/rails_deploy_test.git"
 
-set :use_sudo, false
-set :deploy_via, :copy
+set :use_sudo, true
+set :deploy_via, :checkout
 set :git_shallow_clone, 1
 
 set :user, "deploy"
 set :bundle_without, [:test]
-#default_run_options[:pty] = true
+default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true }
 
 host =  "ec2-54-200-3-110.us-west-2.compute.amazonaws.com"
 role :web, host
