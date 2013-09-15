ea51d06b54bcd833d3a8b6e295b057d477696428
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-15 15:30:09 +0900

    set default env

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,3 +22,13 @@ role :web, host
 role :app, host
 role :db,  host, :primary => true
 
+
+
+set :default_environment, {
+  'FFFFFFFFFFF' => "dsfmslfmlsmdfmsklmfsmdkfm"
+  #'PATH' => "/path/to/.rvm/gems/ree/1.8.7/bin:/path/to/.rvm/bin:/path/to/.rvm/ree-1.8.7-2009.10/bin:$PATH",
+  #'RUBY_VERSION' => 'ruby 1.8.7',
+  #'GEM_HOME'     => '/path/to/.rvm/gems/ree-1.8.7-2010.01',
+  #'GEM_PATH'     => '/path/to/.rvm/gems/ree-1.8.7-2010.01',
+  #'BUNDLE_PATH'  => '/path/to/.rvm/gems/ree-1.8.7-2010.01'  # If you are using bundler.
+}
